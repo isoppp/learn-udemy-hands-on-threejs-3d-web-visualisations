153429f0b32adf78f6bafb85681e85eaaa664485
isoppp <prog@d2deck.com>
2019-11-14 15:09:28 -0800

    add initialized threejs for course1

---
+++ course1/index.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+
+let scene, camera, renderer;
+
+function init () {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement)
+  window.addEventListener('resize', onResize, false)
+
+  update();
+}
+
+function update () {
+  requestAnimationFrame(update);
+  renderer.render(scene, camera);
+}
+
+function onResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.document.addEventListener('DOMContentLoaded', () => {
+  init()
+})
